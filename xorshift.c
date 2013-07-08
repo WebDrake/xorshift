19495e327623296872af9fb599709ca04c7413ec
Joseph Rushton Wakeling <joe@webdrake.net>
2013-07-08 20:49:02 +0200

    Xorshift64 using 32-bit unsigned integer type.
     * copied from Marsaglia (2003) using the first choice
       of [a, b, c] triple, [10, 13, 10].

--- xorshift.c
+++ xorshift.c
@@ -8,6 +8,12 @@ uint32_t xor32()
 	y^=(y<<13); y=(y>>17); return (y^=(y<<5));
 }
 
+uint32_t xor64()
+{
+	static uint32_t x = 123456789, y = 362436069;
+	uint32_t t = (x^(x<<10)); x = y; return y = (y^(y>>10))^(t^(t>>13));
+}
+
 int main(void)
 {
 	size_t i;
@@ -16,6 +22,13 @@ int main(void)
 	for(i=0; i<10; ++i)
 	{
 		printf("  %"PRIu32, xor32());
+	}
+	printf("\n\n");
+
+	printf("Xorshift64:");
+	for(i=0; i<10; ++i)
+	{
+		printf("  %"PRIu32, xor64());
 	}
 	printf("\n");
 
