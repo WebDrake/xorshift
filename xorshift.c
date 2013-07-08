4305bd5a0ac4d94f59505713a085b34d2e4d482e
Joseph Rushton Wakeling <joe@webdrake.net>
2013-07-08 21:18:45 +0200

    Xorwow (192-bit), using a 32-bit unsigned integer type.
      * This is copied direct from p. 5 of Marsaglia (2003).

--- xorshift.c
+++ xorshift.c
@@ -34,6 +34,13 @@ uint32_t xor160()
 	uint32_t t = (x^(x>>2)); x = y; y = z; z = w; w = v; return v = (v^(v>>4))^(t^(t>>1));
 }
 
+uint32_t xorwow()
+{
+	static uint32_t x = 123456789, y = 362436069, z = 521288629,
+	                w = 88675123, v = 5783321, d = 6615241;
+	uint32_t t = (x^(x>>2)); x = y; y = z; z = w; w = v; v = (v^(v<<4))^(t^(t<<1)); return (d+=362437)+v;
+}
+
 int main(void)
 {
 	size_t i;
@@ -70,6 +77,13 @@ int main(void)
 	for(i=0; i<10; ++i)
 	{
 		printf("  %"PRIu32, xor160());
+	}
+	printf("\n\n");
+
+	printf("Xorshift192:");
+	for(i=0; i<10; ++i)
+	{
+		printf("  %"PRIu32, xorwow());
 	}
 	printf("\n");
 
